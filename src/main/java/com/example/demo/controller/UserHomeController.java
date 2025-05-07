be4a201036b9bd70fdc9d6000a303e20dc873ef6
yeji-star <sizuku134@gmail.com>
2025-05-07 11:18:45 +0900

    계산

--- src/main/java/com/example/demo/controller/UserHomeController.java
+++ src/main/java/com/example/demo/controller/UserHomeController.java
@@ -6,9 +6,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
-	@RequestMapping("/user/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕!!";
+	private int i;
+
+	public UserHomeController() {
+		i = 0;
 	}
+
+	@RequestMapping("/user/home/setI")
+	@ResponseBody
+	public String showMain(int value) {
+		this.i = value;
+		
+		return "초기화";
+	}
+
+	@RequestMapping("/user/home/getI")
+	@ResponseBody
+	public int showMain4() {
+
+		return i++;
+
+	}
+
+	@RequestMapping("/user/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가";
+	}
+
+	@RequestMapping("/user/home/main3")
+	@ResponseBody
+	public int showMain3() {
+
+		return 1 + 2;
+	}
+
 }
