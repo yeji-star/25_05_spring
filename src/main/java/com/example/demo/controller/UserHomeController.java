437ce2022a8207fe7856dd27da96d2e0bab194d6
yeji-star <sizuku134@gmail.com>
2025-05-07 11:43:24 +0900

    타입별 출력

--- src/main/java/com/example/demo/controller/UserHomeController.java
+++ src/main/java/com/example/demo/controller/UserHomeController.java
@@ -1,44 +1,86 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UserHomeController {
-	private int i;
+	
 
-	public UserHomeController() {
-		i = 0;
+	@RequestMapping("/user/home/getInt")
+	@ResponseBody
+	public int getInt() {		
+		return 1;
+	}
+	
+	@RequestMapping("/user/home/getString")
+	@ResponseBody
+	public String getString() {		
+		return "abc";
 	}
 
-	@RequestMapping("/user/home/setI")
+	@RequestMapping("/user/home/getBoo")
 	@ResponseBody
-	public String showMain(int value) {
-		this.i = value;
+	public boolean getBoo() {		
+		return true;
+	}
+	
+	@RequestMapping("/user/home/getDou")
+	@ResponseBody
+	public double getDou() {		
+		return 3.14;
+	}
+	
+	@RequestMapping("/user/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수 나이", 11);
+		return map;
+	}
+	
+	@RequestMapping("/user/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수 나이");
 		
-		return "초기화";
+		return list;
 	}
-
-	@RequestMapping("/user/home/getI")
+	
+	@RequestMapping("/user/home/getArticle")
 	@ResponseBody
-	public int showMain4() {
-
-		return i++;
-
+	public Article getArticle() {
+		Article article = new Article(1, "제목1", "내용1");
+		
+		
+		return article;
 	}
+	
+}
 
-	@RequestMapping("/user/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "잘가";
-	}
-
-	@RequestMapping("/user/home/main3")
-	@ResponseBody
-	public int showMain3() {
-
-		return 1 + 2;
-	}
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Article {
+	int id;
+	String title;
+	String body;
+	
+//	public Article(int id, String title, String body) {
+//		this.id = id;
+//		this.title = title;
+//		this.body = body;
+//	}
+	
 }
